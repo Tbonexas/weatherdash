9a71a732f797ba401fed7822822ed6c941043bea
Todd Murdoch <TheClash090@gmail.com>
2020-09-13 12:58:13 -0700

    Added switch statement for each weather type tracked: Rain, Drizzle, Mist, etc

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,21 @@
 // Variables to not be reassigned // 
 const appId = '8a595c36a18b2738713e53c678c45e59';
 const units = 'imperial';
-// remove "zip" after log completes // 
-const searchMethod = 'zip';
+// search method variable // 
+let searchMethod;
+
+// function to determine zipcode or city name passing searchTerm // 
+function getSearchMethod(searchTerm){
+     if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+     searchMethod = 'zip';
+     else
+     searchMethod = 'q';
+}
+
 
 function searchWeather(searchTerm){
 //URL calls information from API to return JSON // 
+    getSearchMethod(searchTerm);
     fetch(`https://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${appId}&units${units}`).then(result => {
     return result.json();
     }).then(result => {
@@ -15,7 +25,23 @@ function searchWeather(searchTerm){
 }
 
 function init(resultFromServer) {
-    // console.log(resultFromServer.list);
+    // parsing JSON to get background for weather currently happening with switch statement to each case // 
+    // console.log(resultFromServer);
+    switch(resultFromServer.weather[0, 1, 2, 3, 4].main){
+        case 'Clear':
+            break;
+        case 'Clouds':
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            
+            break;
+        case '':
+        default:
+            break;
+    }
 }
 // click function for search button // 
 document.getElementById('searchBtn').addEventListener('click', () =>{
