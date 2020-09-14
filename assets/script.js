33bf82d8f93e6ffaa26d018a8a26c7039fdaa267
Todd Murdoch <TheClash090@gmail.com>
2020-09-13 21:19:58 -0700

    added jpg for each weather function. Finish script tomorrow. *work on forecast over 1 day*

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 // Using Vanilla JS for all script // 
+
 // Variables to not be reassigned // 
 const appId = '8a595c36a18b2738713e53c678c45e59';
 const units = 'imperial';
@@ -17,7 +18,7 @@ function getSearchMethod(searchTerm){
 function searchWeather(searchTerm){
 //URL calls information from API to return JSON // 
     getSearchMethod(searchTerm);
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${appId}&units${units}`).then(result => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units${units}`).then(result => {
     return result.json();
     }).then(result => {
         init(result);
@@ -27,16 +28,26 @@ function searchWeather(searchTerm){
 function init(resultFromServer) {
     // parsing JSON to get background for weather currently happening with switch statement to each case // 
     // results from server for first 5 in array for forecast // 
-    switch(resultFromServer.weather[0, 1, 2, 3, 4].main){
+    switch (resultFromServer.weather[0].main) {
         case 'Clear':
+            // this will generate image for clear sky // 
+            document.body.style.backgroundImage = 'url("https://github.com/Tbonexas/weatherdash/blob/master/assets/Clearskies.jpeg?raw=true")';
             break;
         case 'Clouds':
+            document.body.style.backgroundImage = 'url("Cloudy.jpeg")';
             break;
 
         case 'Rain':
         case 'Drizzle':
         case 'Mist':
+            document.body.style.backgroundImage = 'url("Rain.jpeg")';
+            break;
 
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("Storm.jpeg")';
+            break;
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("Snow.jpeg")';
             break;
         case '':
         default:
